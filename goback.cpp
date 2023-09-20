6f68881d44de5f182116106f471e2aefb15bc28a
pankaj1204 <141701152+pankaj1204@users.noreply.github.com>
2023-09-20 08:44:01 +0530

    Update goback.cpp

--- goback.cpp
+++ goback.cpp
@@ -10,15 +10,14 @@ struct Frame {
     int data;
 };
 
-int n;
 vector<Frame> frames;
 
-// Function to initialize sender frames
-void initializeSenderFrames() {
+void initializeFrames() {
+    int n;
     cout << "Enter the number of frames to be sent: ";
     cin >> n;
 
-    frames.resize(n + 1); // Resize the vector to accommodate frames from 1 to n
+    frames.resize(n + 1);
 
     for (int i = 1; i <= n; i++) {
         cout << "Enter data for frame " << i << ": ";
@@ -27,10 +26,9 @@ void initializeSenderFrames() {
     }
 }
 
-// Function to simulate acknowledgment reception
 int simulateAcknowledgment() {
     srand(time(NULL));
-    int r = rand() % n + 1; // Random frame number between 1 and n
+    int r = rand() % frames.size();
 
     cout << "Simulating acknowledgment for frame " << r << endl;
 
@@ -42,7 +40,20 @@ int simulateAcknowledgment() {
     return -1;
 }
 
-// Function to resend frames using Selective Repeat ARQ
+void resendFramesGoBackN(int r) {
+    if (r == -1) {
+        cout << "No frame to resend." << endl;
+        return;
+    }
+
+    cout << "Resending frames from frame " << r << endl;
+
+    for (int i = r + 1; i < frames.size(); i++) {
+        frames[i].ack = 'y';
+        cout << "Received data of frame " << i << " is " << frames[i].data << endl;
+    }
+}
+
 void resendFramesSelectiveRepeat(int r) {
     if (r == -1) {
         cout << "No frame to resend." << endl;
@@ -53,54 +64,33 @@ void resendFramesSelectiveRepeat(int r) {
     cout << "Received data of frame " << r << " is " << frames[r].data << endl;
 }
 
-// Function to resend frames using Go-Back-N ARQ
-void resendFramesGoBackN(int r) {
-    if (r == -1) {
-        cout << "No frame to resend." << endl;
-        return;
-    }
-
-    cout << "Resending frames from frame " << r << endl;
-
-    for (int i = r; i <= n; i++) {
-        frames[i].ack = 'y';
-        cout << "Received data of frame " << i << " is " << frames[i].data << endl;
-    }
-}
-
-// Function to implement Go-Back-N ARQ
-void goBackN() {
-    initializeSenderFrames();
-    int r = simulateAcknowledgment();
-    resendFramesGoBackN(r);
-    cout << "All frames sent successfully." << endl;
-}
-
-// Function to implement Selective Repeat ARQ
-void selectiveRepeat() {
-    initializeSenderFrames();
-    int r = simulateAcknowledgment();
-    resendFramesSelectiveRepeat(r);
-    cout << "All frames sent successfully." << endl;
-}
-
 int main() {
     int choice;
+
     do {
-        cout << "\n\n1. Selective Repeat ARQ\n2. Go-Back-N ARQ\n3. Exit\n";
+        cout << "\n1. Go-Back-N ARQ\n2. Selective Repeat ARQ\n3. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
 
         switch (choice) {
             case 1:
-                selectiveRepeat();
+                initializeFrames();
+                int r1 = simulateAcknowledgment();
+                resendFramesGoBackN(r1);
+                cout << "All frames sent successfully using Go-Back-N ARQ." << endl;
                 break;
+
             case 2:
-                goBackN();
+                initializeFrames();
+                int r2 = simulateAcknowledgment();
+                resendFramesSelectiveRepeat(r2);
+                cout << "All frames sent successfully using Selective Repeat ARQ." << endl;
                 break;
+
             case 3:
                 cout << "Exiting the program." << endl;
                 break;
+
             default:
                 cout << "Invalid choice. Please try again." << endl;
         }
